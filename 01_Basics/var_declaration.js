cfa1e59bbf602218b7913e9cbf6930b255321138
Abhishek Rajaram Jagtap <officialabhishek3000@gmail.com>
2024-04-03 06:54:33 +0000

    var

---
+++ 01_Basics/var_declaration.js
@@ -0,0 +1,9 @@
+const emp_id=121
+
+let emp_name="Ab"
+var emp_sal=25000.35
+
+console.table([emp_id,emp_name,emp_sal])
+//it will take arg as an array so u have to enclosed it within a square brackets.
+
+
